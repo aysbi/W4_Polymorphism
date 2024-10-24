97df8148c00084732887c1ff8ede68f0a7840776
Aysel Bilmez <aybilmez@gmail.com>
2024-10-24 18:23:34 +0300

    Proje dosyası ekle.

---
+++ W4_Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4_Polymorphism
+{
+    public class BaseGeometrikSekil  //base sinif olarak tanimladik
+    {
+        public int Yukseklik {  get; set; } //3 geometrik seklin de yukseklik ve genisligi var bu degerler hepsi icin gecerli
+        public int Genislik { get; set; }
+
+        public virtual int AlanHesapla() //alan hesaplama icin ortak formul virtual olarak kaydettik cunku override yapacagiz dikucgen icin
+        {
+            return Yukseklik*Genislik;
+        }
+    }
+    public class Dikdortgen : BaseGeometrikSekil // icine ekstra bir sey tanimlamamiza gerek yok base sinifin ayni ozelliklerine sahip
+    {
+    }
+    public class Kare : BaseGeometrikSekil //dikdortgen ile ayni ozelliklerde
+    {
+    }
+    public class DikUcgen : BaseGeometrikSekil //ucgen alani farkli hesplaniyor 
+    {
+        public override int AlanHesapla() //virtual olan metodu override yapip ekleme yapiyoruz sonucu 2ye boluyoruz 
+        {
+            return base.AlanHesapla()/2;
+        }
+    }
+}
---
+++ W4_Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using W4_Polymorphism;
+
+BaseGeometrikSekil Ucgen = new DikUcgen(); 
+Ucgen.Genislik = 4;
+Ucgen.Yukseklik = 3;
+
+Console.WriteLine("Ucgenin Alani: " + Ucgen.AlanHesapla()); //ucgen alanini yazdiriyoruz
+
+Dikdortgen dikdortgen = new Dikdortgen();
+dikdortgen.Genislik = 5;
+dikdortgen.Yukseklik = 6;
+
+Console.WriteLine("Dikdortgenin alani: " + dikdortgen.AlanHesapla()); //dikdortgen alanini yazdiriyoruz
+
+Kare kare = new Kare();
+kare.Genislik = 7;
+kare.Yukseklik = 7;
+
+Console.WriteLine("Karenin alani: " + kare.AlanHesapla()); //karenin alanini yazdiriyoruz
+
+Console.ReadKey();
